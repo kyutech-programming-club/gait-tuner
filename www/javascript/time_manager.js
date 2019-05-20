2ccb7f3b2b103f59714cdb81eacacfc137c988a6
tanacchi <q111026d@mail.kyutech.jp>
2019-05-20 17:28:17 +0900

    Add comments on time_manager.js

--- www/javascript/time_manager.js
+++ www/javascript/time_manager.js
@@ -13,17 +13,22 @@ TimeManager.prototype = {
     this.addTime = 0;
   },
 
+  // フォームから移動速度を取得する
+  // だがしかし使ってない
   changeSpeedPara: function() {
     var param = document.forms['speedparaform'].speedpara.value;
     this.defaultSpeed.walk = param;
     console.log(this.defaultSpeed.walk);
   },
 
+  // 現在時刻を文字列形式にして出力
   getCurrentTime: function() {
     var date = new Date();
     return date.getHours() + ":" + ("0"+(date.getMinutes())).slice(-2);
   },
 
+  // 目標時刻をフォームから取得し
+  // Dateオブジェクトに変換する
   getTargetTime: function() {
     var date = new Date();
     var form = document.forms['target_time_form']
@@ -35,13 +40,16 @@ TimeManager.prototype = {
     return new Date(td[0], td[1]-1, td[2], tmp[0], tmp[1]);
   },
 
+  // 出発時刻（~分後）が変更されたときに
+  // フォームから値を取り出して
+  // 残り時間を再計算する
   addStartTime: function() {
     this.addTime = document.forms['start_time_form'].start_time.value;
     console.log(this.addTime);
     this.calcRemainingTime();
   },
 
-
+  // 残り時間[秒]を計算する
   calcRemainingTime: function() {
     var startDate = new Date();
     startDate.setMinutes(startDate.getMinutes() + this.addTime);
@@ -50,6 +58,8 @@ TimeManager.prototype = {
     this.remainingTime = (this.getTargetTime().getTime() - startDate.getTime()) / 1000;
   },
 
+  // 目標時刻のデフォルトの値をフォームに埋め込む
+  // 大半がHTMLが解釈できるフォーマットへの変換
   setDefaultTargetTime: function() {
     var currentDate = new Date();
     var y0 = ("0000" + (currentDate.getFullYear())).slice(-4);
